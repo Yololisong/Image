f4981fc8cb9f2c49eb3b164dcdef5d738e0f2299
Yololisong <123672161@qq.com>
2024-10-08 16:33:41 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
       <div >
-        <p className="text-xs text-gray-500">请勿上传违反中国法律的图片，违者后果自负！Powered by
+        <p className="text-xs text-gray-500">请勿上传违反中国法律的图片，违者后果自负！© 2020 Powered by
           <Link 
           href="https://www.cloudflare.com/"
           className="text-blue-300  hover:text-red-900 ml-1"
